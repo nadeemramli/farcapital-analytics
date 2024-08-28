93edc538cea39f94f112d41b3495248e09826594
nadeemramli <m.nadeemramli@gmail.com>
2024-08-29 06:20:37 +0800

    fix the admin.py and mage port issue

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import (
+    AcquisitionMetrics,
+    ActivationMetrics,
+    AwarenessMetrics,
+    CampaignPerformance,
+    CustomerJourney,
+    MarketingActivity,
+    ReferralMetrics,
+    RetentionMetrics,
+    RevenueMetrics
+)
 
-# Register your models here.
+admin.site.register(AcquisitionMetrics)
+admin.site.register(ActivationMetrics)
+admin.site.register(AwarenessMetrics)
+admin.site.register(CampaignPerformance)
+admin.site.register(CustomerJourney)
+admin.site.register(MarketingActivity)
+admin.site.register(ReferralMetrics)
+admin.site.register(RetentionMetrics)
+admin.site.register(RevenueMetrics)
